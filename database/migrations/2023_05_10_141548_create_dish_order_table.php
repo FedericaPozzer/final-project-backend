86e414d9f173d1ec257fc2c932446bbfaa4c23fc
Alessandro Montenero <alemonte99@icloud.com>
2023-05-12 17:59:11 +0200

    commit

--- database/migrations/2023_05_10_141548_create_dish_order_table.php
+++ database/migrations/2023_05_10_141548_create_dish_order_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreignId('order_id');
             $table->integer('quantity')->nullable();
             $table->timestamps();
+            
         });
     }
 
