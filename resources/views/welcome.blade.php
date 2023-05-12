6fc3a1242dab7e3d3544084bf7f0de0d74f31f16
Alessandro Montenero <alemonte99@icloud.com>
2023-05-12 16:55:21 +0200

    some validations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 @section('content')
-
-@if ()
-    
+@if (auth()->user())
+@if (auth()->user()->restaurant)
+    <a type="button" class="btn btn-success border fw-bold" href="{{route('restaurants.edit', auth()->user()->restaurant)}}">Modifica il tuo ristorante</a>
 @else
-    
+    <a type="button" class="btn btn-success border fw-bold" href="{{route('restaurants.create')}}">Registra il tuo ristorante</a>
+@endif  
+@else  
+registrati
 @endif
-<a type="button" class="btn btn-success border fw-bold" href="{{route('restaurants.create')}}">Registra il tuo ristorante</a>
 
 @endsection
