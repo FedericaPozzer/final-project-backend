0311588d77f0357f32ea5864b60f6cd71b96b7a8
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-05-15 11:06:53 +0200

    validation restaurant form

--- resources/views/restaurant/form.blade.php
+++ resources/views/restaurant/form.blade.php
@@ -16,6 +16,8 @@ Creazione e modifica ristorante
     <h2>crea il tuo ristorante</h2>
 @endif
 
+@include('layouts.partials.errors')
+
 {{-- * se il ristorante esiste già form edit / se il ristorante non esiste già form create --}}
 @if ($restaurant->id)
     <form action="{{route('restaurants.update', $restaurant)}}" method="POST" class="row">
@@ -28,26 +30,49 @@ Creazione e modifica ristorante
     {{-- * nome ristorante --}}
     <div class="col-8 my-4">
         <label class="form-label" for="name">Nome Ristorante</label>
-        <input type="text" name="name" id="name" class="form-control">
+        <input type="text" name="name" id="name" class="form-control @error("name") is-invalid @enderror" value="{{ old("name") ?? $restaurant->name }}">
+        @error("name")
+            <div class="invalid-feedback"> {{ $message }} </div>
+        @enderror
     </div>
-    
+
+
     {{-- * indirizzo --}}
     <div class="col-8 my-4">
         <label class="form-label" for="address">Indirizzo</label>
-        <input type="text" name="address" id="address" class="form-control">
+        <input type="text" name="address" id="address" class="form-control @error("address") is-invalid @enderror" value="{{ old("address") ?? $restaurant->address }}">
+        @error("address")
+            <div class="invalid-feedback"> {{ $message }} </div>
+        @enderror
     </div>
-    
+
     {{-- * partita iva --}}
     <div class="col-8 my-4">
         <label class="form-label" for="vat">P. IVA</label>
-        <input type="text" name="vat" id="vat" class="form-control">
+        <input type="text" name="vat" id="vat" class="form-control @error("vat") is-invalid @enderror" value="{{ old("vat") ?? $restaurant->vat }}">
+        @error("vat")
+            <div class="invalid-feedback"> {{ $message }} </div>
+        @enderror
     </div>
     
     {{-- * numero di telefono --}}
     <div class="col-8 my-4">
         <label class="form-label" for="phone_number">Numero di Tel.</label>
-        <input type="text" name="phone_number" id="phone_number" class="form-control">
+        <input type="text" name="phone_number" id="phone_number" class="form-control @error("phone_number") is-invalid @enderror" value="{{ old("phone_number") ?? $restaurant->phone_number }}">
+        @error("phone_number")
+            <div class="invalid-feedback"> {{ $message }} </div>
+        @enderror
     </div>
+
+     {{-- * immagine --}} 
+        {{-- TODO: image! --}}
+        <div class="col-8 my-4">
+          <label class="form-label" for="image">Immagine</label>
+          <input type="text" name="image" id="image" class="form-control @error("image") is-invalid @enderror" value="{{ old("image") ?? $restaurant->image }}">
+          @error("image")
+              <div class="invalid-feedback"> {{ $message }} </div>
+          @enderror
+      </div>
 
     {{-- * EDIT / CREATE submit --}}
     @if ($restaurant->id)
