e4100060915449a289d92770779437343bbdd77c
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-05-16 17:12:33 +0200

    creation show card

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -3,22 +3,36 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-10">
+    <div class="row mt-5">
+        <div class="col-8 offset-2">
             <div class="card">
-                <h3>Dettaglio piatto - show</h3>  
-                <p>
-                    {{$dish->name}}
-                </p>
-                <p>
-                    {{$dish->price}}
-                </p>
-                <p>
-                    {{$dish->description}}
-                </p>
-            </div>
+                <div class="row basic-card-row">
+                    <div class="col-sm-12 col-md-8">
+                        <img src="https://www.burgerking.it/site/assets/files/8495654/bacon_king_3_0-1.png" class="card-img-top img-card-dish" alt="dish_image">
+                    </div>
+                    <div class="col-sm-10 col-md-4">
+                        <h3 class="my-4">Prezzo: &euro; {{$dish->price}}</h3>
+                        <h4 class="my-4">{{$dish->available == 1 ? 'Disponibile' : 'Non disponibile'}}</h4>
+                    </div>
+                </div>
+                    
+                <div class="card-body">
+                  <h1 class="card-title my-4">I dettagli del tuo piatto "{{$dish->name}}"</h1>
+                  <p class="fw-bold my-4">
+                      {{$dish->description}}
+                  </p>
+                  <a href="{{ route('dashboard') }}" class="btn btn-primary">Torna al tuo menu</a>
+                </div>
+              </div>
         </div>
 
+                        
+    
+        
+        
+        
+        
+        
 
 
 
