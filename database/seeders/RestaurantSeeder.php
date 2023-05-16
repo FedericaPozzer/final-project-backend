142ace7c7d52333ce219d73680993b5056ee2787
Alessandro Montenero <alemonte99@icloud.com>
2023-05-16 15:23:48 +0200

    push

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Type;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class RestaurantSeeder extends Seeder
 {
@@ -49,6 +50,38 @@ class RestaurantSeeder extends Seeder
             /* Se l'utente non ha ristoranti gliene associo uno */
             if (!(User::first()->restaurant)){
                 $restaurant->owner()->associate(User::first());
+                $restaurant->save();
+                $restaurant->types()->save(Type::all()->random(1)->first());
+            }
+
+            for($i=0; $i<5; $i++)
+            {
+                $user = new User;
+                $user->name = $faker->name();
+                $user->password = Hash::make("password");
+                $user->email = $faker->email();
+                $user->save();
+
+                $prefix = $prefixes[$faker->numberBetween(0, 2)];
+                $street = $streets[$faker->numberBetween(0, 4)];
+                $number = $faker->numberBetween(1, 50);
+
+                // nome ristorante (company o "Da" + nome proprio)
+                if($faker->boolean(50)){
+                    $name = $faker->company();
+                }
+                else{
+                    $name =  'Da ' . $faker->firstName() ;
+                }
+        
+                /* Creo un nuovo ristorante */
+                $restaurant = new Restaurant;
+                $restaurant->name = $name;
+                $restaurant->address = $prefix . ' ' . $street . ' ' . $number;
+                $restaurant->vat = $faker->randomNumber(9, true);
+                $restaurant->phone_number = $faker->phoneNumber();
+
+                $restaurant->owner()->associate($user);
                 $restaurant->save();
                 $restaurant->types()->save(Type::all()->random(1)->first());
             }
