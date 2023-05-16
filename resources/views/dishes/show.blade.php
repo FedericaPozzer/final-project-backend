29a7b87f75c2756117b69ec2fdeeb3eacda57f14
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-05-16 17:49:11 +0200

    showshow

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -11,7 +11,7 @@
                     <div class="col-sm-12 col-md-6">
                         <img src="https://www.burgerking.it/site/assets/files/8495654/bacon_king_3_0-1.png" class="card-img-top img-card-dish" alt="dish_image">
                     </div>
-                    <div class="col-sm-10 col-md-6 d-flex flex-column justify-content-evenly">
+                    <div class="col-sm-10 col-md-6 d-flex flex-column justify-content-between">
                         <h3 class="my-4">Prezzo: &euro; {{$dish->price}}</h3>
                         <h4 class="my-4">{{$dish->available == 1 ? 'Disponibile' : 'Non disponibile'}}</h4>
                         <p class="fw-bold my-4">
